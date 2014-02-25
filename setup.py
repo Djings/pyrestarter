8d55f5ffc781dbbf151e2d6537173c2c19faa4ec
Djings <rogkor@gmail.com>
2014-02-25 12:42:04 +0100

    added setup.py to install script into system path
     - removed .py extension from script name
     - set snakefood call to only include internal libraries to speed up
       dependency checking

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyrestarter',
+  author = "Djings",
+  author_email = "rogkor@gmail.com",
+  license = "MIT",
+  scripts = [
+      'pyrestarter',
+  ]
+)
+
