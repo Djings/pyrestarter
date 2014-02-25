1ce5a08d3051fafc308eaa943e4017e34e311ed3
Djings <rogkor@gmail.com>
2014-02-25 13:12:34 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name = 'pyrestarter',
-  version = '0.12',
+  version = '0.13',
   description = 'restart python scripts when sources change',
   author = "Djings",
   author_email = "rogkor@gmail.com",
