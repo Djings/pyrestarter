bb119ac86855c045856baf9f6927cd11ee6d49ab
Djings <rogkor@gmail.com>
2014-02-25 13:07:25 +0100

    added option for global vs. local dependency tracking

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from distutils.core import setup
 
 setup(
   name = 'pyrestarter',
+  version = '0.12',
+  description = 'restart python scripts when sources change',
   author = "Djings",
   author_email = "rogkor@gmail.com",
+  url = 'https://github.com/Djings/pyrestarter',
   license = "MIT",
   scripts = [
       'pyrestarter',
